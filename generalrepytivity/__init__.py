d333b621d84fcc9589cf6b24a02565edae5e4919
Miguel González Duque <miguelgondu@gmail.com>
2018-01-12 15:47:52 -0500

    First release

--- generalrepytivity/__init__.py
+++ generalrepytivity/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Miguel Gonzalez Duque"""
 __email__ = 'miguelgondu@gmail.com'
-__version__ = '__version__ = '0.1.0''
+__version__ = '__version__ = '__version__ = '0.1.0'''
 
 
 import itertools
